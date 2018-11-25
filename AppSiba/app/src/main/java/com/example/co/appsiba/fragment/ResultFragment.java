774047075167ed814e18355d7a1839830ee84ed7
seonhui <h613@naver.com>
2018-11-25 23:27:59 +0900

    sh1125

--- AppSiba/app/src/main/java/com/example/co/appsiba/fragment/ResultFragment.java
+++ AppSiba/app/src/main/java/com/example/co/appsiba/fragment/ResultFragment.java
@@ -1,15 +1,25 @@
 package com.example.co.appsiba.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.co.appsiba.R;
+import com.example.co.appsiba.result.TabPagerAdapter;
 
 public class ResultFragment extends Fragment {
 
+
+    private  View view;
+    protected   ViewPager viewPager;
+    protected TabLayout tabLayout;
+    private  TabPagerAdapter pagerAdapter;
+    private  int image;
     public ResultFragment() {
     }
 
@@ -22,19 +32,53 @@ public class ResultFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_results, container, false);
+        view = inflater.inflate(R.layout.fragment_results, container, false);
+
+
+       tabLayout = (TabLayout)view.findViewById(R.id.tabs) ;
+        viewPager = (ViewPager) view.findViewById(R.id.resultViewPager);
+        pagerAdapter = new TabPagerAdapter(getChildFragmentManager());
+
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
 
 
+        viewPager.addOnPageChangeListener( new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
 
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
     }
 
 }
