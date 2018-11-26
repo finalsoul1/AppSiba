716fda2c892ec54e30f4b609e1a8bae21d3e293e
greenery <elindreams@hanmail.net>
2018-11-26 10:59:47 +0900

    .

--- query/favorates.sql
+++ query/favorates.sql
@@ -1,6 +1,6 @@
 -- 즐겨찾기 메뉴 목록  
 select a.food_id, b.name, b.small_image_location
 from my_favorates a left outer join food b
-on a.food_id = b.id;
-
+on a.food_id = b.id
+order by a.reg_date desc;
 
