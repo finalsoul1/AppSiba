1d6567f024564a69093b48a0562340c977f7e738
greeneries <42959261+greeneries@users.noreply.github.com>
2018-11-27 20:20:02 +0900

    Update recommend_food.sql

--- query/recommend_food.sql
+++ query/recommend_food.sql
@@ -1,6 +1,10 @@
 
 Collections.shuffle(fileList);
 
+-- 내 내장고 속 재료 가져오기 
+select b.name from my_refrigerator a left outer join ingredient_list b
+on a.ingredient_list_id = b.id
+
 -- 추천메뉴 
 select id, name, small_image_location, big_image_location
 from (
