a748c6e90c95acff16da0a67755ca500d2574bc3
finalsoul1 <42801415+finalsoul1@users.noreply.github.com>
2018-11-27 18:26:03 +0900

    kwon

--- query/favorates.sql
+++ query/favorates.sql
@@ -8,6 +8,8 @@ order by a.reg_date desc;
 -- 즐겨찾기 추가 쿼리 
 insert into my_favorates (food_id, reg_date) values (250, now());
 
+-- sqlite 날짜 
+cv.put(db.PROCESS_DATE, new Date().getTime());
 
 -- 즐겨찾기 삭제
 delete from my_favorates where food_id = 250;
