d1d198c2c362895d6b4d3bb96e570f4796d790f4
greeneries <42959261+greeneries@users.noreply.github.com>
2018-11-28 16:34:59 +0900

    Update recommend_food.sql

--- query/recommend_food.sql
+++ query/recommend_food.sql
@@ -7,17 +7,18 @@ on a.ingredient_list_id = b.id;
 
 -- 추천메뉴 
 select id, name, small_image_location
-	select id, name, small_image_location, big_image_location
+from (
+	select id, name, small_image_location
 	from food 
 	where food_type_id = 1
 	and id in (select food_id from food_ingredients where name like '%상추%')
 	union
-	select id, name, small_image_location, big_image_location
+	select id, name, small_image_location
 	from food 
 	where food_type_id = 1
 	and id in (select food_id from food_ingredients where name like '%콩나물%')
 	union
-	select id, name, small_image_location, big_image_location
+	select id, name, small_image_location
 	from food 
 	where food_type_id = 1
 	and id in (select food_id from food_ingredients where name like '%곤드레나물%')
